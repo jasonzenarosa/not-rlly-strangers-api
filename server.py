701989e19a70034a475cce44dcba144464d379bd
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-07 13:22:07 -0800

    Initial commit

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import data, random
+
+app = Flask(__name__)
+
+def random_card(deck):
+    return {"card": random.choice(deck)}
+
+@app.route("/level1")
+def l1():
+    return random_card(data.levelOne)
+
+@app.route("/level2")
+def l2():
+    return random_card(data.levelTwo)
+
+@app.route("/level3")
+def l3():
+    return random_card(data.levelThree)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
