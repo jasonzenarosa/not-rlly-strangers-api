716d2352b1d4409ac5d2ce74788019549c346520
Jason Zenarosa <jasonzenarosa@gmail.com>
2022-11-08 18:18:49 -0800

    Allowed cross origin

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response, request
 import data, random
 
 app = Flask(__name__)
 
+def _build_cors_preflight_response():
+    response = make_response()
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    response.headers.add('Access-Control-Allow-Headers', "*")
+    response.headers.add('Access-Control-Allow-Methods', "*")
+    return response
+
+def _corsify_actual_response(response):
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
 def random_card(deck):
-    return {"card": random.choice(deck)}
+    return _corsify_actual_response(jsonify({"card": random.choice(deck)}))
 
 @app.route("/")
 def index():
@@ -12,15 +23,30 @@ def index():
 
 @app.route("/level1")
 def l1():
-    return random_card(data.levelOne)
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "GET": # The actual request following the preflight
+        return random_card(data.levelOne)
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
 
 @app.route("/level2")
 def l2():
-    return random_card(data.levelTwo)
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "GET": # The actual request following the preflight
+        return random_card(data.levelOne)
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
 
 @app.route("/level3")
 def l3():
-    return random_card(data.levelThree)
+    if request.method == "OPTIONS": # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "GET": # The actual request following the preflight
+        return random_card(data.levelOne)
+    else:
+        raise RuntimeError("Weird - don't know how to handle method {}".format(request.method))
 
 if __name__ == "__main__":
     app.run(threaded=True, port=8000)
